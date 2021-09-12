fa4d2237a1c44b853e5f54bb99e37aa3d90f11a1
zhw he <1025675485@qq.com>
2021-09-12 22:33:06 +0800

    Spring事务管理基于注解

--- src/main/java/com/he_zhw/DaoImpl/AccountDaoImpl.java
+++ src/main/java/com/he_zhw/DaoImpl/AccountDaoImpl.java
@@ -1,13 +1,22 @@
 package com.he_zhw.DaoImpl;
 
 import com.he_zhw.Dao.AccountDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+
+@Repository
 public class AccountDaoImpl extends JdbcDaoSupport implements AccountDao {
 
+    @Autowired
+    public void setDaoDataSource(DataSource DataSource) {
+        this.setDataSource(DataSource);
+    }
 
     @Override
-    public int updateAccountById(int id,double money) {
+    public int updateAccountById(int id, double money) {
 
 /*        UPDATE account
         SET
@@ -15,7 +24,7 @@ public class AccountDaoImpl extends JdbcDaoSupport implements AccountDao {
         NAME = ? ，
         money = ? WHERE id = ?
         ;*/
-        String sql="update account set money=money+? where id=?";
-        return this.getJdbcTemplate().update(sql,money,id);
+        String sql = "update account set money=money+? where id=?";
+        return this.getJdbcTemplate().update(sql, money, id);
     }
 }
--- src/main/java/com/he_zhw/serviceImpl/AccountServiceImpl.java
+++ src/main/java/com/he_zhw/serviceImpl/AccountServiceImpl.java
@@ -2,15 +2,19 @@ package com.he_zhw.serviceImpl;
 
 import com.he_zhw.Dao.AccountDao;
 import com.he_zhw.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service("AccountService")
 public class AccountServiceImpl implements AccountService {
-    private AccountDao accountDao=null;
 
-    public void setAccountDao(AccountDao accountDao) {
-        this.accountDao = accountDao;
-    }
+    @Autowired
+    private AccountDao accountDao;
 
     @Override
+    @Transactional(propagation = Propagation.REQUIRED)
     public int transfer(int TransferorID, int recipientId, double money) {
         int i = accountDao.updateAccountById(TransferorID, -100);
         //int j=1/0;
